4b04adac4d732bae499ecb8f6b6241fd8585027a
singhpartap <babupartap48@gmail.com>
2018-06-12 22:05:31 +0530

    assignmnet 2

---
+++ assignment2.py
@@ -0,0 +1,21 @@
+#Q1:
+print("Hello my name is Patap Singh")
+
+
+#Q2: 
+print("Partap" "Singh")
+
+
+#Q3:
+x=input("Enter the value of x")
+y=input("Enter the value of y")
+z=input("Enter the value of z")
+print(x,y,z)
+
+#Q4:
+print('"let\'s get started"')
+#Q5:
+s="acadview"
+course="python"
+fee="5000"
+print(('%s\n%s\n%s')%(s, course, fee))
---
+++ practice.py
@@ -0,0 +1,4 @@
+x=4
+y=6
+z=x+y
+print(x)
