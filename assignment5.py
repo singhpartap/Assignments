d8704f95799d194ac0c213d9d5c765728e825636
singhpartap <babupartap48@gmail.com>
2018-06-15 14:26:21 +0530

    assignment 5

--- assignment5.py
+++ assignment5.py
@@ -11,22 +11,75 @@ else :
 
 
 
-
-
-
-
-
-
-	  #Q2
+#Q2
 	  
 	  
 	  
-	x=int(input("enter value of length :"))
+x=int(input("enter value of length :"))
 
 y=int(input("enter value of breadth :"))
 
-if (x/y==1) :
+if (x==y):
     print("diamensions are of square")
 	
 else :
     print("diamensions are of rectangle")
+	
+	
+	
+#Q3
+
+
+x=int(input("enter age of x:"))
+
+
+y=int(input("enter age of y :"))
+
+z=int(input("enter age of z :"))
+
+if x>y and x>z :
+	if y>z:
+		print("x is older and z is younger")
+	else:
+		print("x is older and y is younger")
+if y>z and y>x :
+	if x>z:
+		print("y is older and z is younger")
+	else:
+		print("y is older and x is younger")
+if z>y and z>x :
+	if y>x:
+		print("z is older and x is younger")
+	else:
+		print("z is older and y is younger")
+
+	
+	
+#Q4
+
+p=int(input("enter points :"))
+
+if p<=50 :
+	print(" no prize")
+elif p>=51 and p<=150:
+	print("wooden prize")
+elif p>=151 and p<=180:
+	print("book")
+elif p>=181 and p<=200 :
+	print("chocolates")
+else:
+	print(" invalid input")
+	
+	
+	
+#Q5
+
+
+n=int(input("enter cost:"))
+
+if n>=1000:
+	n=n-n*0.1
+	print(n)
+else :
+	print(n)	
+	
